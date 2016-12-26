b4d5edff62b2bca2175e7bcd153129c786fb0d68
Jovi Kamdani <grim00@gmail.com>
2016-12-27 00:20:01 +0800

    New devs

---
+++ vlt.js
@@ -0,0 +1,9 @@
+// VLT.JS for VIDRA
+// Author: Jovi Kamdani
+// jovi.tv
+
+// Nav Bar Loader
+// var navHTML = $.get('navbar.html');
+// $(document).ready(function() {
+//     $('#navigation').append(navHTML)
+// });
